47fa58b49986021eb323bc6cd0b0a00e8ecfa7d3
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-26 20:31:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.size == 1
 elsif array.size == 2 
    array.join(" and ")
 else array.size >= 3
-  array.insert(array.last,"and ")
+  array.last.insert(-1,"and ")
   array.join(", ")
 end
 end
